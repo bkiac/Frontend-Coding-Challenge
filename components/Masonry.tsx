077cf1d314a98ae75fcfe7eab6bbf62803e96362
Ben Knáb <bkiac@pm.me>
2021-12-21 13:42:38 +0100

    Remove redundant props from the masonry component

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -2,7 +2,7 @@ import {Box, Img} from "@chakra-ui/react"
 import type {GIFObject} from "giphy-api"
 
 export const Masonry: React.VFC<{gifs: GIFObject[]}> = ({gifs}) => (
-	<Box w="100%" mx="auto" sx={{columnCount: [1, 2, 3, 4, 5], columnGap: 2}}>
+	<Box sx={{columnCount: [1, 2, 3, 4, 5], columnGap: 2}}>
 		{gifs.map(({id, title, images: {fixed_width}}) => (
 			<Img
 				key={id}
